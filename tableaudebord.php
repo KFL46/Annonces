6ed16b4653a9e93ef718fec1639fe1c7649bcdf3
FAUGERAS <kflmada@gmail.com>
2020-04-08 18:26:03 +0200

    validation TDB

--- tableaudebord.php
+++ tableaudebord.php
@@ -1,13 +1,32 @@
 <?php
+		$dbh = new PDO
+		(
+			'mysql:host=localhost;dbname=petitesannonces;charset=utf8',
+			'root',
+			'',
+			[
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+			]
+		);
+	
+	session_start();
 
-session_start();
-//	Si l'utilisateur n'est pas authentifié
-if(!array_key_exists('petitesannonces', $_SESSION))
-{
-	//	Redirection vers la page d'accueil
-	echo("Le nom d'utilisateur ou le mot de passe est incorrect !");
-	header('Location: ./');
-	exit;
-}
-//	Inclusion du HTML
-include 'tableaudebord.phtml';
+	//	Si l'utilisateur n'est pas authentifié
+	if(!array_key_exists('petitesannonces', $_SESSION)){
+				//Redirection vers la page d'accueil
+		header('Location: ./');
+			exit;
+	}
+
+// //////////////////// RECUPERATION DES DONNEES DE LA BDD ////////////////////////////
+//On sélectionne les annonces 
+	$query='SELECT * FROM annonces WHERE idutilisateur = ?';
+	$sth = $dbh->prepare($query);
+	$sth -> bindValue(1,($_SESSION['petitesannonces']), PDO::PARAM_INT);
+	$sth -> execute();
+	$annonces = $sth->fetchAll();
+	var_dump($annonces); // vérification affichage annonce
+
+	//	Inclusion du HTML
+	include 'tableaudebord.phtml';
