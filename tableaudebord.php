9474da4a6b7782b5ac59d27d1018b6e652190a93
FAUGERAS <kflmada@gmail.com>
2020-04-08 14:47:34 +0200

    tableau de bord

--- tableaudebord.php
+++ tableaudebord.php
@@ -1,30 +1,13 @@
 <?php
 
-	session_start();
-
-	//	Si l'utilisateur n'est pas authentifié
-	if(!array_key_exists('petitesannonces', $_SESSION))
-	{
-		//	Redirection vers la page d'accueil
-		header('Location: ./');
-		exit;
-	}
-// //////////////////// RECUPERATION DES DONNEES DE LA BDD ////////////////////////////
-// ///////////////////////////////////////////////////////////////////////////////////
-	//On sélectionne les articles d'un id après authentification
-	$query = 'INSERT INTO annonces (annonce, image, ) VALUES (?, ?)';
-	$sth = $dbco->prepare($query);
-	$sth->bindValue(1, trim($_GET['id']), PDO::PARAM_INT);
-	$sth->bindValue(3, trim($_SESSION['petitesannonces']));
-	var_dump($_SESSION);
-	$sth-> execute();
-   
-	$resultat = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-	echo '<pre>';
-	print_r($resultat);
-	echo '</pre>';
-
-
-	//	Inclusion du HTML
-	include 'tableaudebord.phtml';
+session_start();
+//	Si l'utilisateur n'est pas authentifié
+if(!array_key_exists('petitesannonces', $_SESSION))
+{
+	//	Redirection vers la page d'accueil
+	echo("Le nom d'utilisateur ou le mot de passe est incorrect !");
+	header('Location: ./');
+	exit;
+}
+//	Inclusion du HTML
+include 'tableaudebord.phtml';
