5d73f8ccc266c7fb71cb577a20f6bc7e876b907d
FAUGERAS <kflmada@gmail.com>
2020-04-17 17:23:14 +0200

    deconnexion annonces

--- deconnexion.php
+++ deconnexion.php
@@ -1,24 +1,23 @@
 <?php
- 
+         session_start();
+         session_destroy();
+         header('Location: index.phtml');
+
     // On démarre la session
-    session_start();
  
-    if isset($_SESSION['petitesannonces']) { // Si tu es connecté on te déconnecte et on te redirige vers une page.
+   // if (isset($_SESSION['petitesannonces']) { // Si tu es connecté on te déconnecte et on te redirige vers une page.
  
         // Supression des variables de session et de la session
-        $_SESSION = array();
-        session_destroy();
+       // $_SESSION = array();
  
         // Supression des cookies de connexion automatique
         //setcookie('utilisateur', '');
         //setcookie('passwordhache', '');
          
-        //header('Location: index.php');
  
-    }else{ // Dans le cas contraire on t'empêche d'accéder à cette page en te redirigeant vers la page que tu veux.
+    //}else{ // Dans le cas contraire on t'empêche d'accéder à cette page en te redirigeant vers la page que tu veux.
  
-        header('Location: authentification.php');
  
-    }
+  //  });
  
 ?>
